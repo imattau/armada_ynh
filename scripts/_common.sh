d73469d16521ce4b59d301da38c4f224b91d08e4
lostcause <lostcause@pop-os.lan>
2026-09-02 19:43:44 +1000

    Fix package linter findings

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,11 +5,9 @@ app_root="${install_dir:-/var/www/$app}"
 build_dir="$app_root/dist"
 
 build_armada() {
-    ynh_script_progression --message="Installing Armada frontend dependencies..." --weight=2
     pushd "$app_root" >/dev/null
     npm ci --ignore-scripts
 
-    ynh_script_progression --message="Building Armada frontend..." --weight=3
     VITE_APP_NAME="$app_name" \
     VITE_APP_RELAYS="$app_relays" \
     VITE_SEARCH_RELAYS="$search_relays" \
